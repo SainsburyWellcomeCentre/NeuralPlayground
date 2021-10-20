b306e2a9ab0c932058bc993e98055eec65d84cec
clementine <clementine.domine98@gmail.com>
2021-10-20 13:43:37 +0100

    making the dimention work

--- model/basic.py
+++ model/basic.py
@@ -2,7 +2,7 @@ import sys
 sys.path.append("../")
 import numpy as np
 import random
-from neuroscience_models_prosecutor.model.core import NeuralResponseModel as NeurResponseModel
+from model.core import NeuralResponseModel as NeurResponseModel
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -19,14 +19,14 @@ class ExcitInhibitoryplastic(NeurResponseModel):
         self.sigma_exc= mod_kwargs["sigma_exc"]
         self.alpha_inh= mod_kwargs["alpha_inh"]
         self.sigma_inh= mod_kwargs["sigma_inh"]
-        self.D = len(self.sigma_exc)  # Stimulus dimensions: punctate and contextual cues.
+        self.D = 1  # Stimulus dimensions: punctate and contextual cues.
         self.reset()
 
     def reset(self):
         self.global_steps = 0
         self.history = []
-        self.wi = np.ones((self.D,self.Ni)) #what is the mu and why do we have the 1 and not2
-        self.we = np.ones((self.D,self.Ne))
+        self.wi = np.ones((self.Ni)) #what is the mu and why do we have the 1 and not2
+        self.we = np.ones((self.Ne))
         rout = np.zeros((self.D,1))
  
     def act(self, observation):
@@ -36,19 +36,19 @@ class ExcitInhibitoryplastic(NeurResponseModel):
 
     def update(self, x):
         self.global_steps += 1
-        self.get_rates_exc= self.alpha_exc*np.exp((x/self.sigma_exc)**2)
+        self.get_rates_exc= self.alpha_exc*np.exp((x.T@x/self.sigma_exc))
         self.get_rates_exc_dup = np.tile(self.get_rates_exc,(self.Ne,1))
-        self.get_rates_inh= self.alpha_inh*np.exp((x/self.sigma_inh)**2)
+        self.get_rates_inh= self.alpha_inh*np.exp((x.T@x/self.sigma_inh))
         self.get_rates_inh_dup = np.tile(self.get_rates_inh,(self.Ni,1))
         self.rout = (np.dot(self.we,self.get_rates_exc_dup)- np.dot(self.wi,self.get_rates_inh_dup))
-        print(self.we.shape)
-        print(self.get_rates_exc_dup.shape)
-        print(self.rout.shape)
-        self.we = self.we + self.etaexc @ self.get_rates_exc_dup @ self.rout    # Weight update inh
-        self.wi = self.wi +  self.etainh @  self.get_rates_inh_dup @ self.rout  # Weight update exc
-        transition = {"wi": self.wi , "we": self.we, "r_out": rout,}
+        a=self.etaexc * self.get_rates_exc_dup @ self.rout
+        print(a.shape)
+        self.we = self.we + self.etaexc * self.get_rates_exc_dup @ self.rout    # Weight update inh
+        self.wi = self.wi +  self.etainh *  self.get_rates_inh_dup @ self.rout  # Weight update exc
+        transition = {"wi": self.wi , "we": self.we, "self.rout": self.rout,}
         self.history.append(transition)
-        return rout
+        
+        return self.rout
     
 
     
