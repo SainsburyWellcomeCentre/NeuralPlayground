13b7e9232bb9515dd8817dbc9d4823ea4b8d95d0
clementine <clementine.domine98@gmail.com>
2021-10-20 21:38:08 +0100

    place cellsssssssss

--- model/basic.py
+++ model/basic.py
@@ -22,28 +22,46 @@ class ExcitInhibitoryplastic(NeurResponseModel):
         self.sigma_inh= mod_kwargs["sigma_inh"]
         self.D = 1  # Stimulus dimensions: punctate and contextual cues.
         self.reset()
+        self.rates_exc =[] ;
 
     def reset(self):
         self.global_steps = 0
         self.history = []
         self.wi = np.ones((self.Ni)) #what is the mu and why do we have the 1 and not2
         self.we = np.ones((self.Ne))
-        rout = np.zeros((self.D,1))
+        self.rout = np.zeros((self.D,1))
  
     def act(self, observation):
         action=np.random.normal(scale=0.1, size=(2,))
         return action
  
+    def get_rates_exc(self, x):
+        self.rates_exc=np.zeros((self.Ne,1))
+        for i in range(self.Ne):
+            mean = np.random.normal(1)
+            mean = mean # This mean should be calculated in a smarter way.
+            rates_exc_i=self.alpha_exc*np.exp(((x-mean).T@(x-mean)/self.sigma_exc))
+            self.rates_exc[i]= rates_exc_i
+        return self.rates_exc
+        
+    def get_rates_inh(self, x):
+        self.rates_inh= np.zeros((self.Ni,1))
+        for i in range(self.Ni):
+            mean = np.random.normal(1)
+            mean = mean
+            rates_inh_i= self.alpha_inh*np.exp(((x-mean).T@(x-mean)/self.sigma_inh))
+            self.rates_inh[i]=rates_inh_i
+        return self.rates_inh
+        
+    def output_rates(self, x):
+        self.get_rates_exc_dup= self.get_rates_exc(x)
+        self.get_rates_inh_dup= self.get_rates_inh(x)
+        self.rout = (np.dot(self.we,self.get_rates_exc_dup)- np.dot(self.wi,self.get_rates_inh_dup))
+        return self.rout
 
     def update(self, x):
         self.global_steps += 1
-        self.get_rates_exc= self.alpha_exc*np.exp((x.T@x/self.sigma_exc))
-        self.get_rates_exc_dup = np.tile(self.get_rates_exc,(self.Ne,1))
-        self.get_rates_inh= self.alpha_inh*np.exp((x.T@x/self.sigma_inh))
-        self.get_rates_inh_dup = np.tile(self.get_rates_inh,(self.Ni,1))
-        self.rout = (np.dot(self.we,self.get_rates_exc_dup)- np.dot(self.wi,self.get_rates_inh_dup))
-        a=self.etaexc * self.get_rates_exc_dup @ self.rout
-        print(a.shape)
+        self.rout=self.output_rates(x)
         self.we = self.we + self.etaexc * self.get_rates_exc_dup @ self.rout    # Weight update inh
         self.wi = self.wi +  self.etainh *  self.get_rates_inh_dup @ self.rout  # Weight update exc
         transition = {"wi": self.wi , "we": self.we, "self.rout": self.rout,}
@@ -51,37 +69,46 @@ class ExcitInhibitoryplastic(NeurResponseModel):
         
         return self.rout
         
-    def plot_rate(self,colormap='viridis',number_of_different_colors=30,):
-        output_rates =
-        linspace = np.linspace(-0.5 , 0.5, 51)
-        X, Y = np.meshgrid(linspace, linspace)
-        distance = np.sqrt(X*X + Y*Y)
-        print(output_rates.shape)
-        title = 'Plot of rate'
-        plt.title(title, fontsize=12)
-        cm = getattr(mpl.cm, colormap)
-        cm.set_over('y', 1.0) # Set the color for values higher than maximum
-        cm.set_bad('white', alpha=0.0)
-        maximal_rate = int(np.ceil(np.amax(output_rates)))
-        V = np.linspace(0, maximal_rate, number_of_different_colors)
-        # Hack to avoid error in case of vanishing output rate at every position
-        # If every entry in output_rates is 0, you define a norm and set
-        # one of the elements to a small value (such that it looks like zero)
-        if np.count_nonzero(output_rates) == 0:
-            color_norm = mpl.colors.Normalize(0., 100.)
-            output_rates[0][0] = 0.000001
-            V = np.linspace(0, 2.0, number_of_different_colors)
-            plt.contourf(X, Y, output_rates[...,0], V, norm=color_norm, cmap=cm, extend='max')
-        else:
-        a = output_rates[:, :, 28, 0]
-        plt.contourf(X, Y, a, V, cmap=cm)
-        plt.margins(0.01)
-        plt.axis('off')
-        ticks = np.linspace(0.0, maximal_rate, 2)
-        plt.colorbar(format='%.2f', ticks=ticks)
-        ax = plt.gca()
-        self.set_axis_settings_for_contour_plots(ax)
-        return
+    def plot_rate(self,room_width, room_depth, colormap='viridis',number_of_different_colors=30,):
+             arena_limits = np.array([[-room_width/2, room_width/2],
+             [-room_depth/2, room_depth/2]])
+             # I would like to put as input the width of the env
+             figsize=(8, 6)
+             linspace_width = np.linspace(-room_width/2, room_width/2, 20)
+             linspace = np.linspace(-room_depth/2, room_depth/2, 20)
+             X, Y = np.meshgrid(linspace_width, linspace)
+             a=np.zeros((len(X),len(X)))
+             U= np.zeros((1,2))
+             for i in range(len(X)):
+                 for j in range(len(X.T)):
+                     U=np.array([X[i,j],Y[i,j]])
+                     a[i,j]=self.output_rates(U)
+             title = 'Plot of rate'
+             plt.title(title, fontsize=12)
+             cm = getattr(mpl.cm, colormap)
+             cm.set_over('y', 1.0) # Set the color for values higher than maximum
+             cm.set_bad('white', alpha=0.0)
+             maximal_rate = int(np.ceil(np.amax(a)))
+             V = np.linspace(0, maximal_rate, number_of_different_colors)
+             # Hack to avoid error in case of vanishing output rate at every position
+             # If every entry in output_rates is 0, you define a norm and set
+             # one of the elements to a small value (such that it looks like zero)
+             plt.contourf(X, Y, a, V, cmap=cm)
+             plt.margins(0.01)
+             plt.axis('off')
+             ticks = np.linspace(0.0, maximal_rate, 2)
+             plt.colorbar(format='%.2f', ticks=ticks)
+             ax = plt.gca()
+             ax.plot([-room_width/2, room_width/2],
+                     [-room_depth/2, -room_depth/2], "r", lw=2)
+             ax.plot([-room_width/2, room_width/2],
+                     [room_depth/2, room_depth/2], "r", lw=2)
+             ax.plot([-room_width/2, -room_width/2],
+                     [-room_depth/2, room_depth/2], "r", lw=2)
+             ax.plot([room_width / 2, room_width / 2],
+                     [-room_depth / 2, room_depth / 2], "r", lw=2)
+             plt.show()
+             return X
     
 
     
