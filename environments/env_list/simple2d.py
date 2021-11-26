f67a55c19e77f09b1e3b02d96a05e590b851072e
rodrigcd <rodrigo.carrasco.davis@gmail.com>
2021-11-26 16:33:57 +0000

    refactor of simple model done, need to solve problem for firing rate scales

--- environments/env_list/simple2d.py
+++ environments/env_list/simple2d.py
@@ -135,6 +135,8 @@ class Sargolini2006(Simple2D):
         self.data = FullSargoliniData(data_path=self.data_path, experiment_name=self.environment_name, verbose=verbose, session=session)
         self.arena_limits = self.data.arena_limits
         self.room_width, self.room_depth = np.abs(np.diff(self.arena_limits, axis=1))
+        self.room_width = self.room_width[0]
+        self.room_depth = self.room_depth[0]
         env_kwargs["room_width"] = self.room_width
         env_kwargs["room_depth"] = self.room_depth
         env_kwargs["agent_step_size"] = 1/50  # In seconds
@@ -157,6 +159,8 @@ class Sargolini2006(Simple2D):
     def step(self, action):
         """ Action is ignored in this case """
         self.global_steps += 1
+        if self.global_steps >= self.data.position.shape[0]-1:
+            self.global_steps = 0
         self.global_time = self.global_steps*self.agent_step_size
         reward = 0  # If you get reward, it should be coded here
         new_state = self.data.position[self.global_steps, :], self.data.head_direction[self.global_steps, :]
--- model/basic.py
+++ model/basic.py
@@ -7,184 +7,178 @@ from model.core import NeuralResponseModel as NeurResponseModel
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 import numpy as np
-import math
+from scipy.stats import multivariate_normal
+from environments.env_list.simple2d import Simple2D, Sargolini2006
+from tqdm import tqdm
 
 
-class ExcitInhibitoryplastic(NeurResponseModel):
+class ExcInhPlasticity(NeurResponseModel):
 
     def __init__(self, model_name="ExcitInhibitoryplastic", **mod_kwargs):
         super().__init__(model_name, **mod_kwargs)
-        self.agent_step_size= mod_kwargs["agent_step_size"]
+        self.agent_step_size = mod_kwargs["agent_step_size"]
         self.metadata = {"mod_kwargs": mod_kwargs}
         self.etaexc = mod_kwargs["exc_eta"]  # Learning rate.
         self.etainh = mod_kwargs["inh_eta"]
         self.Ne = mod_kwargs["Ne"]
         self.Ni = mod_kwargs["Ni"]
-        self.alpha_exc = mod_kwargs["alpha_exc"]
+        self.Nef = mod_kwargs["Nef"]
+        self.Nif = mod_kwargs["Nif"]
+
         self.sigma_exc = mod_kwargs["sigma_exc"]
-        self.sigma_exc_1 = mod_kwargs["sigma_exc_1"]
-        self.alpha_inh = mod_kwargs["alpha_inh"]
+
         self.sigma_inh = mod_kwargs["sigma_inh"]
-        self.sigma_inh_1 = mod_kwargs["sigma_inh_1"]
-        self.D = 1  # Stimulus dimensions: punctate and contextual cues.
+        self.room_width, self.room_depth = mod_kwargs["room_width"], mod_kwargs["room_depth"]
+        self.ro = mod_kwargs["ro"]
+        self.obs_history = []
+
+        self.resolution = 50
+        self.x_array = np.linspace(-self.room_width/2, self.room_width/2, num=self.resolution)
+        self.y_array = np.linspace(-self.room_depth/2, self.room_depth/2, num=self.resolution)
+        self.mesh = np.array(np.meshgrid(self.x_array, self.y_array))
+        self.xy_combinations = self.mesh.T.reshape(-1, 2)
+
         self.reset()
-        self.rates_exc = [];
 
     def reset(self):
         self.global_steps = 0
         self.history = []
-        self.wi = np.ones((self.Ni))  # what is the mu and why do we have the 1 and not2
-        self.we = np.ones((self.Ne))
-        self.sumwe = np.sum(self.we ** 2)
-        self.xprev = [0, 0]
+        self.wi = np.random.normal(loc=1.52, scale=1.52*0.05, size=(self.Ni))  # what is the mu and why do we have the 1 and not2
+        self.we = np.random.normal(loc=1.0, scale=1.0*0.05, size=(self.Ne))
 
-    def act(self, observation):
+        self.inh_rates_functions, self.inh_cell_list = self.generate_tuning_curves(n_curves=self.Ni,
+                                                                                   cov_scale=self.sigma_inh,
+                                                                                   Nf=self.Nif)
+        self.exc_rates_functions, self.exc_cell_list = self.generate_tuning_curves(n_curves=self.Ne,
+                                                                                   cov_scale=self.sigma_exc,
+                                                                                   Nf=self.Nef)
+        self.init_we_sum = np.sum(self.we**2)
+
+    def generate_tuning_curves(self, n_curves, cov_scale, Nf):
+        width_limit = self.room_width / 2.0
+        depth_limit = self.room_depth / 2.0
+        cell_list = []
+        function_list = []
+        for i in tqdm(range(n_curves)):
+            gauss_list = []
+            cell_i = 0
+            for j in range(Nf):
+                mean1 = np.random.uniform(-width_limit*(1+0.1), width_limit*(1+0.1))
+                mean2 = np.random.uniform(-depth_limit*(1+0.1), depth_limit*(1+0.1))
+                cov = np.diag([(self.room_width * cov_scale)**2, (self.room_depth * cov_scale)**2])
+                mean = [mean1, mean2]
+                rv = multivariate_normal(mean, cov)
+                function_list.append([mean, cov])
+                cell_i += rv.pdf(self.xy_combinations)
+            function_list.append(gauss_list)
+            norm_cell_i = (cell_i - np.amin(cell_i))/np.amax((cell_i - np.amin(cell_i)))
+            cell_list.append(norm_cell_i)
+        return function_list, np.array(cell_list)
+
+    def act(self, obs):
+        self.obs_history.append(obs)
         action = np.random.normal(scale=0.1, size=(2,))
         return action
 
-    def get_rates_exc(self, x, room_width, room_depth):
-        rates_exc_save = np.zeros((self.Ne, 1))
-        for i in range(self.Ne):
-            A = round((room_width/2)*1000)
-            B = round((room_depth / 2) * 1000)
-            mean1 = np.random.randint(-A,A)*0.001
-            mean2 = np.random.randint(-B, B) * 0.001
-            mean=[mean1, mean2]
-            mean=np.array(mean)
-            l_exc=(x-mean)
-            sig_exc=[self.sigma_exc_1,self.sigma_exc]
-            sigma=np.diag(np.array(sig_exc))
-            rates_exc_i = self.alpha_exc * np.exp(l_exc.T @ sigma@ (l_exc))
-            rates_exc_save[i] = rates_exc_i
-        return rates_exc_save
+    def get_output_rates(self, pos):
+        exc_rates = self.get_rates(self.exc_cell_list, pos)
+        inh_rates = self.get_rates(self.inh_cell_list, pos)
 
-    def get_rates_inh(self, x, room_width, room_depth, ):
-        rates_inh_save = np.zeros((self.Ni, 1))
-        for i in range(self.Ni):
-            A = round((room_width / 2) * 1000)
-            B = round((room_depth / 2) * 1000)
-            mean1 = np.random.randint(-A, A) * 0.001
-            mean2 = np.random.randint(-B, B) * 0.001
-            mean = [mean1, mean2]
-            mean = np.array(mean)
-            l_inh = (x - mean)
-            sig_inh = [self.sigma_inh_1, self.sigma_inh]
-            sigma_inh = np.diag(np.array(sig_inh))
-            rates_inh_i = self.alpha_inh * np.exp(l_inh.T @ sigma_inh @ (l_inh))
-            rates_inh_save[i] = rates_inh_i
-        return rates_inh_save
+        r_out = self.we.T @ exc_rates - self.wi.T @ inh_rates
+        # print("debug")
+        return np.abs(r_out)
 
-    def get_rates_exc_select(self, x, room_width, number_to_select, room_depth):
-        rates_exc_save = np.zeros((number_to_select, 1))
-        for i in range(number_to_select):
-            A = round((room_width / 2) * 1000)
-            B = round((room_depth / 2) * 1000)
-            mean1 = np.random.randint(-A, A) * 0.001
-            mean2 = np.random.randint(-B, B) * 0.001
-            mean=[mean1, mean2]
-            mean=np.array(mean)
-            l_exc=(x-mean)
-            sig_exc=[self.sigma_exc_1,self.sigma_exc]
-            sigma=np.diag(np.array(sig_exc))
-            rates_exc_i = self.alpha_exc * np.exp(l_exc.T @ sigma@ (l_exc))
-            rates_exc_save[i] = rates_exc_i
-        return rates_exc_save
+    def get_rates(self, cell_list, pos, get_n_cells=None):
+        diff = self.xy_combinations - pos[np.newaxis, ...]
+        dist = np.sum(diff**2, axis=1)
+        index = np.argmin(dist)
+        rout = []
+        for i in range(cell_list.shape[0]):
+            rout.append(cell_list[i, index])
+        return np.abs(rout)
 
-    def get_rates_inh_select(self, x, room_width, Number_to_select, room_depth, ):
-        rates_inh_save = np.zeros(( Number_to_select, 1))
-        for i in range( Number_to_select):
-            A = round((room_width / 2) * 1000)
-            B = round((room_depth / 2) * 1000)
-            mean1 = np.random.randint(-A, A) * 0.001
-            mean2 = np.random.randint(-B, B) * 0.001
-            mean = [mean1, mean2]
-            mean = np.array(mean)
-            l_inh = (x - mean)
-            sig_inh = [self.sigma_inh_1, self.sigma_inh]
-            sigma_inh = np.diag(np.array(sig_inh))
-            rates_inh_i = self.alpha_inh * np.exp(l_inh.T @ sigma_inh @ (l_inh))
-            rates_inh_save[i] = rates_inh_i
-        return rates_inh_save
+    def get_full_output_rate(self):
+        r_out = self.we.T @ self.exc_cell_list - self.wi.T @ self.inh_cell_list
+        r_out = r_out.reshape((self.resolution, self.resolution))
+        r_out = np.abs(r_out)
+        return np.abs(r_out)
 
+    def update(self, exc_normalization=True):
+        pos = self.obs_history[-1]
+        r_out = self.get_output_rates(pos)
 
-    def output_rates(self, x, room_width, room_depth):
-        self.get_rates_exc_dup = self.get_rates_exc(x, room_width, room_depth)
-        self.get_rates_inh_dup = self.get_rates_inh(x, room_width, room_depth)
-        rout = (np.dot(self.we, self.get_rates_exc_dup) - np.dot(self.wi, self.get_rates_inh_dup))
-        if rout < 0:
-            rout=[0]
-            rout=np.array(rout)
-        print(rout)
-        return rout
+        delta_we = self.etaexc*self.get_rates(self.exc_cell_list, pos=pos)
+        delta_wi = self.etainh*self.get_rates(self.inh_cell_list, pos=pos)*(r_out - self.ro)
 
-    def output_rates_select(self,x,room_width, room_depth):
-        Number_to_select=1
-        self.get_rates_exc_dup_select = self.get_rates_exc(x, room_width, Number_to_select, room_depth)
-        self.get_rates_inh_dup_select = self.get_rates_inh(x, room_width, Number_to_select,  room_depth)
-        rout = (np.dot(self.we, self.get_rates_exc_dup) - np.dot(self.wi, self.get_rates_inh_dup))
-        if rout < 0:
-            rout = [0]
-            rout = np.array(rout)
-        print(rout)
-        return rout
+        # print(delta_wi, delta_we)
 
-    def update(self, action, room_width, room_depth):
-        action = action / np.linalg.norm(action)
-        x= self.xprev + self.agent_step_size * action
-        x = np.array([np.clip(x[0], a_min=-room_width / 2, a_max=room_width / 2),
-                              np.clip(x[1], a_min=-room_depth / 2, a_max=room_depth / 2)])
+        self.we = self.we + delta_we
+        self.wi = self.wi + delta_wi
 
-        self.global_steps += 1
-        rout = self.output_rates(x, room_width, room_depth)
-        self.we = self.we + self.etaexc * self.get_rates_exc_dup @ rout  # Weight update inh
-        self.wi = self.wi + self.etainh * self.get_rates_inh_dup @ rout  # Weight update exc
-        l= (self.sumwe/sum((self.we))**2)
-        self.we=self.we*l
-        transition = {"wi": self.wi, "we": self.we, "self.rout": rout, }
-        self.history.append(transition)
-        self.xprev=x
-        return rout
+        if exc_normalization:
+            self.we = self.init_we_sum/np.sum(self.we**2)*self.we
 
-    def plot_rate(self, room_width, room_depth, colormap='viridis', number_of_different_colors=30, ):
-        arena_limits = np.array([[-room_width / 2, room_width / 2],
-                                 [-room_depth / 2, room_depth / 2]])
-        # I would like to put as input the width of the env
-        figsize = (8, 6)
-        linspace_width = np.linspace((-room_width / 2), (room_width / 2), 30)
-        linspace = np.linspace((-room_depth / 2), (room_depth / 2), 30)
-        X, Y = np.meshgrid(linspace_width, linspace)
-        a = np.zeros((len(X), len(X)))
-        U = np.zeros((1, 2))
-        for i in range(len(X)):
-            for j in range(len(Y)):
-                U = np.array([X[i, j], Y[i, j]])
-                a[i,j] = self.output_rates(U, room_width, room_depth)
-        title = 'Plot of rate'
-        plt.title(title, fontsize=12)
-        cm = getattr(mpl.cm, colormap)
-        print(a)
-        cm.set_over('y', 1.0)  # Set the color for values higher than maximum
-        cm.set_bad('white', alpha=0.0)
-        maximal_rate = int(np.ceil(np.amax(a)))
-        V = np.linspace(0, maximal_rate, number_of_different_colors)
-        # Hack to avoid error in case of vanishing output rate at every position
-        # If every entry in output_rates is 0, you define a norm and set
-        # one of the elements to a small value (such that it looks like zero)
-        plt.contourf(X, Y, a, V, cmap=cm)
-        plt.margins(0.01)
-        plt.axis('off')
-        ticks = np.linspace(0.0, maximal_rate, 2)
-        plt.colorbar(format='%.2f', ticks=ticks)
-        ax = plt.gca()
-        ax.plot([-room_width / 2, room_width / 2],
-                [-room_depth / 2, -room_depth / 2], "r", lw=2)
-        ax.plot([-room_width / 2, room_width / 2],
-                [room_depth / 2, room_depth / 2], "r", lw=2)
-        ax.plot([-room_width / 2, -room_width / 2],
-                [-room_depth / 2, room_depth / 2], "r", lw=2)
-        ax.plot([room_width / 2, room_width / 2],
-                [-room_depth / 2, room_depth / 2], "r", lw=2)
+        self.we = np.abs(self.we)
+        self.wi = np.abs(self.wi)
+
+    def plot_rates(self):
+        f, ax = plt.subplots(1, 3, figsize=(14, 5))
+
+        r_out_im = self.get_full_output_rate()
+        exc_im = self.exc_cell_list[0, ...].reshape((self.resolution, self.resolution))
+        inh_im = self.inh_cell_list[0, ...].reshape((self.resolution, self.resolution))
+
+        ax[0].imshow(exc_im, cmap="Reds")
+        ax[0].set_title("Exc rates", fontsize=14)
+        ax[1].imshow(inh_im, cmap="Blues")
+        ax[1].set_title("Inh rates", fontsize=14)
+        im = ax[2].imshow(r_out_im)
+        ax[2].set_title("Out rate", fontsize=14)
+        cbar = plt.colorbar(im, ax=ax[2])
         plt.show()
-        return X
 
-    
+
+if __name__ == "__main__":
+    # Create an env
+    data_path = "/home/rodrigo/HDisk/8F6BE356-3277-475C-87B1-C7A977632DA7_1/all_data/"
+
+    session = {"rat_id": "11016", "sess": "31010502"}
+
+    env = Sargolini2006(data_path=data_path,
+                        verbose=False,
+                        session=session,
+                        time_step_size=None,
+                        agent_step_size=None)
+
+    exc_eta = 2e-5
+    inh_eta = 8e-5
+    model_name = "model_example"
+    sigma_exc = 0.05
+    sigma_inh = 0.1
+    Ne = 4900
+    Ni = 1225
+    Nef = 100
+    Nif = 100
+    agent_step_size = 0.1
+
+    print("init cells")
+    agent = ExcInhPlasticity(model_name=model_name, exc_eta=exc_eta, inh_eta=inh_eta, sigma_exc=sigma_exc,
+                             sigma_inh=sigma_inh, Ne=Ne, Ni=Ni, agent_step_size=agent_step_size, ro=1,
+                             Nef=Nef, Nif=Nif, room_width=env.room_width, room_depth=env.room_depth)
+    print("Plotting rate")
+    agent.plot_rates()
+
+    print("running updates")
+    n_steps = 30000
+    # Initialize environment
+    obs, state = env.reset()
+    for i in tqdm(range(n_steps)):
+        # Observe to choose an action
+        obs = obs[:2]
+        action = agent.act(obs)
+        rate = agent.update()
+        # Run environment for given action
+        obs, state, reward = env.step(action)
+
+    print("plotting results")
+    agent.plot_rates()
