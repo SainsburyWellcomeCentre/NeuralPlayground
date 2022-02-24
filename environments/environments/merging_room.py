e2d7aa37f6572a5310ff82364d102dd732a67557
Luke Hollingsworth <luke.hollingsworth.21@ucl.ac.uk>
2022-02-24 14:45:58 +0000

    additions to merging room environment

--- environments/environments/merging_room.py
+++ environments/environments/merging_room.py
@@ -1,43 +1,70 @@
 import sys
 sys.path.append("../")
-import matplotlib as mpl
 import matplotlib.pyplot as plt
-from environments.core import Environment
 import numpy as np
-from environments.experiments.merge_room_data import WernleData
 from simple2d import Simple2D
 
-class Merging2D(Simple2D):
-    def __init__(self, data_path="C:\Users\Coursework\Documents\MSc Machine Learning\Project\nn_Data+Code\data", environment_name="MergingRoom2D", **env_kwargs):
-        self.data_path = data_path
+class MergingRoom2D(Simple2D):
+    def __init__(self, environment_name="MergingRoom", **env_kwargs):
         self.environment_name = environment_name
-        self.data = WernleData(data_path=self.data_path, experiment_name=self.environment_name)
-        self.AB_limits = np.array([[0, 200], [-100, 100]])
-        self.A_limits = np.array([[0, 200], [0, 100]])
-        self.B_limits = np.array([[0, 200], [0, 100]])
-        env_kwargs["agent_step_size"] = 1/50  # In seconds
+        self.arena_limits = np.array([[0, self.room_width],
+                                      [-self.room_depth/2, self.room_depth/2]])
+        limits = np.abs(np.diff(self.arena_limits, axis=1))
+        self.room_width = limits[0, 0]
+        self.room_depth = limits[1, 0]
+        env_kwargs["room_width"] = self.room_width
+        env_kwargs["room_depth"] = self.room_depth
+        env_kwargs["agent_step_size"] = 1/50                              
         super().__init__(environment_name, **env_kwargs)
         self.metadata["doi"] = "https://doi.org/10.11582/2017.00023"
-        self.total_number_of_steps = self.data.position.shape[0]
-        self.state_dims_labels = ["x_pos", "y_pos"]
 
-        def get_room(room):
-            if room == "AB":
-                limits = np.abs(np.diff(self.AB_limits, axis=1))
-                self.room_width = limits[0, 0]
-                self.room_depth = limits[1, 0]
+class RandomAgent(object):
+    
+    def act(self, observation):
+        return np.random.normal(scale=0.1, size=(2,))
 
-            if room == "A":
-                limits = np.abs(np.diff(self.A_limits, axis=1))
-                self.room_width = limits[0, 0]
-                self.room_depth = limits[1, 0]
+if __name__ == "__main__":
+    env_name = ["merging_room_envA", "merging_room_envA", "merging_room_envA"]
+    room_width = 200
+    room_depth = [100, 100, 200]
+    time_step_size = 0.1
+    agent_step_size = 0.5
 
-            if room == "B":
-                limits = np.abs(np.diff(self.B_limits, axis=1))
-                self.room_width = limits[0, 0]
-                self.room_depth = limits[1, 0]
+    envA = MergingRoom2D(environment_name=env_name[0],
+               room_width = room_width,
+               room_depth = room_depth[0],
+               time_step_size = time_step_size,
+               agent_step_size = agent_step_size)
+    envB = MergingRoom2D(environment_name=env_name[1],
+               room_width = room_width,
+               room_depth = room_depth[1],
+               time_step_size = time_step_size,
+               agent_step_size = agent_step_size)
+    envAB = MergingRoom2D(environment_name=env_name[2],
+               room_width = room_width,
+               room_depth = room_depth[2],
+               time_step_size = time_step_size,
+               agent_step_size = agent_step_size)
 
-            env_kwargs["room_width"] = self.room_width
-            env_kwargs["room_depth"] = self.room_depth
+    envs = [envA, envB, envAB]
 
-            return self.room_width, self.room_depth
+    agent = RandomAgent()
+
+    n_steps = 1000
+
+    for env in envs:
+        # Initialize environment
+        obs, state = env.reset()
+        for i in range(n_steps):
+            # Observe to choose an action
+            action = agent.act(obs)
+            # Run environment for given action
+            obs, state, reward = env.step(action)
+
+    ax = env.plot_trajectory()
+    fontsize = 16
+    ax.grid()
+    # ax.legend(fontsize=fontsize, loc="upper left")
+    ax.set_xlabel("width", fontsize=fontsize)
+    ax.set_ylabel("depth", fontsize=fontsize)
+    plt.show()
