48f98eb291c5b641f677aeb82bd15a1441b2e085
rodrigcd <rodrigo.carrasco.davis@gmail.com>
2022-03-09 12:52:54 +0000

    package built

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="sehec",
+    version="0.0.1",
+    author="clem_domine_rod_carrasco",
+    author_email="rodrigo.carrasco.davis@gmail.com",
+    description="A Standardized Environment for Hippocampus and Entorhinal Cortex models",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ClementineDomine/EHC_model_comparison",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
