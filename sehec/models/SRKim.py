3c0381386efc301f804b9c5baf4d3660373ba486
clementine <clementine.domine98@gmail.com>
2022-04-14 21:27:54 +0100

    test+commended SR

--- sehec/models/SRKim.py
+++ sehec/models/SRKim.py
@@ -1,8 +1,15 @@
 """
-Implementation for SR Kim
+Implementation for SR 2017 by Kimberly L. Stachenfeld1,2,*, Matthew M. Botvinick1,3, and Samuel J. Gershman4
+The hippocampus as a predictive map
+https://doi.org/10.1101/097170;
+
+This implementation can interact with environments from the package as shown in the examples jupyter notebook.
+Check examples/SRexample.ipynb
 """
+
 import sys
 sys.path.append("../")
+
 import numpy as np
 import random
 import matplotlib.pyplot as plt
@@ -11,29 +18,96 @@ from scipy.stats import multivariate_normal
 from tqdm import tqdm
 
 from .modelcore import NeuralResponseModel
-from ..envs.arenas.simple2d import Simple2D
+from ..envs.arenas.simple2d import Simple2D, Sargolini2006, BasicSargolini2006
 
 
 class SR(NeuralResponseModel):
     """
-    Attributes
+    Implementation for SR 2017 by Kimberly L. Stachenfeld1,2,*, Matthew M. Botvinick1,3, and Samuel J. Gershman4
+    The hippocampus as a predictive map
+    https://doi.org/10.1101/097170;
 
-    gamma: scalar, discounting factor
-    learning_rate: scalar, scaling the magnitude of the TD update
-    random_state: random state object, e.g. np.random.RandomState(seed))
-    set for reproducibility
-    t_episode: scalar, maximum number of timesteps in one episode
-    n_episode: scalar, number of episodes
-    starting_state: scalar, specifting initial state
+    This implementation can interact with environments from the package as shown in the examples jupyter notebook.
+    Check examples/SRexample.ipynb
+    ----
+    Attributes
+    ---------
+     mod_kwargs : dict
+        Model parameters
+        gamma: scalar,
+            discounting factor
+        learning_rate: scalar,
+            scaling the magnitude of the TD update
+        t_episode: scalar,
+            maximum number of timesteps in one episode
+        threoshold: scalar,
+            upper bound for the update size
+        n_episode: scalar,
+            number of episodes
+        transmat: (n_state, n_state)
+            numpy array, transition matrix
+        room_width: float
+                    room width specified by the environment (see examples/examples/SRexample.ipynb)
+        room_depth: float
+                    room depth specified by the environment (see examples/examples/SRexample.ipynb)
+        state_density: float
+            density of SR-agent states (should be proportional to the step-size)
+        twoD: bool
+                When true creates a (n_state, n_state) transition array for a rectangular 2D state space.
+
 
     Methods
     ---------
+    reset(self):
+        Initialize the successor matrices, normalized transition matrix and observation variables ( history and initialisation)
+    obs_to_state(self, pos):
+        Converts the agent's position in the environment to the agent's position in the SR-agent state space.
+    act: float
+        The base model executes one of four action (up-down-right-left) with equal probability.
+        This is used to move on the rectangular environment states space (transmat).
+    get_T_from_M(self, M):
+        Compute the transition matrix from the computationally simulated successor representation matrix M
+    create_transmat(self, state_density, name_env, plotting_varible=True):
+        Creates the normalised transition matrix for a rectangular environment '2D_env'
+    update_successor_rep(self):
+        Compute the successor representation matrix using geometric sums
+    successor_rep_sum(self):
+        Compute the successor representation using successive additive update
+    update_successor_rep_td_full(self):
+        Compute the successor representation matrix using TD learning
+    update(self):
+        Compute the successor representation matrix using TD learning while interacting with the environement
+    plot_transition(self, matrix, save_path=None, ax=None):
+        Plot the input matrix and compare it to the transition matrix from the rectangular environment states space (rectangular- transmat).
+    plot_eigen(self,matrix, save_path, ax=None):
+        Plot the matrix and the 4 largest modes of its eigen-decomposition
     """
 
     def __init__(self, model_name="SR", **mod_kwargs):
         """
         Parameters
         ----------
+        model_name : str
+            Name of the specific instantiation of the ExcInhPlasticity class
+        mod_kwargs : dict
+            gamma: scalar,
+                 discounting factor
+            learning_rate: scalar,
+                scaling the magnitude of the TD update
+            t_episode: scalar,
+                maximum number of timesteps in one episode
+            threshold: scalar,
+               upper bound for the update size
+            n_episode: scalar,
+                number of episodes
+            twoD: bool
+                When true creates a (n_state, n_state) transition array for a rectangular 2D state space.
+            room_width: float
+                room width specified by the environment (see examples/examples/SRexample.ipynb)
+            room_depth: float
+                room depth specified by the environment (see examples/examples/SRexample.ipynb)
+            state_density: float
+                density of SR-agent states (should be proportional to the step-size)
         """
         super().__init__(model_name, **mod_kwargs)
         self.metadata = {"mod_kwargs": mod_kwargs}
@@ -43,11 +117,14 @@ class SR(NeuralResponseModel):
         self.learning_rate = mod_kwargs["lr_td"]
         self.t_episode = mod_kwargs["t_episode"]
         self.n_episode = mod_kwargs["n_episode"]
-        self.reset()
-
         self.room_width = mod_kwargs["room_width"]
         self.room_depth = mod_kwargs["room_depth"]
         self.state_density = mod_kwargs["state_density"]
+        twoD = mod_kwargs['twoD']
+        self.inital_obs_variable = None
+
+        self.reset()
+        # Variables for the SR-agent state space
         self.resolution_d = int(self.state_density * self.room_depth)
         self.resolution_w = int(self.state_density * self.room_width)
         self.x_array = np.linspace(-self.room_width / 2, self.room_width / 2, num=self.resolution_d)
@@ -57,32 +134,42 @@ class SR(NeuralResponseModel):
         self.w = int(self.room_width * self.state_density)
         self.l = int(self.room_depth * self.state_density)
         self.n_state = int(self.l * self.w)
-        self.srmat = []
-        self.srmat_sum = []
-        self.srmat_ground = []
-        self.transmat_norm = []
         self.obs_history = []
-        twoD=mod_kwargs['twoD']
         if twoD==True:
             self.create_transmat(self.state_density,  '2D_env')
 
 
 
 
+
     def reset(self):
         """
-        Initialize
+        Initialize the successor matrices, normalized transition matrix and observation variables (history and initialisation)
         """
+
         self.srmat = []
         self.srmat_sum = []
         self.srmat_ground = []
         self.transmat_norm = []
-        self.w = 0
-        self.l= 0
-        self.global_steps = 0  # Reset global steps
+        self.inital_obs_variable = None
         self.obs_history = []  # Reset observation history
 
     def obs_to_state(self, pos):
+        """
+        Converts the agent's position in the environment to the agent's position in the SR-agent state space.
+
+        Parameters
+        ----------
+        pos: array (2,1)
+            array containing the observed position of the agent in the environment
+
+        Returns
+        -------
+        curr_state: int
+            integer corresponding to the position in the SR-agent state space
+
+
+        """
         node_layout = np.arange(self.n_state).reshape(self.l, self.w)
         diff = self.xy_combinations - pos[np.newaxis, ...]
         dist = np.sum(diff ** 2, axis=1)
@@ -91,6 +178,18 @@ class SR(NeuralResponseModel):
         return curr_state
 
     def act(self,obs):
+        """
+        The base model executes one of four action (up-down-right-left) with equal probability.
+        This is used to move on the rectangular environment states space (transmat).
+        Parameters
+        ----------
+        obs: array (2,1)
+            Observation from the environment class needed to choose the right action (Here the position).
+        Returns
+        -------
+        action : array (2,1)
+            Action value (Direction of the agent step) in this case executes one of four action (up-down-right-left) with equal probability.
+        """
         self.obs_history.append(obs)
         if len(self.obs_history) >= 1000:
             self.obs_history = [obs, ]
@@ -100,32 +199,49 @@ class SR(NeuralResponseModel):
         dist = np.sum(diff ** 2, axis=1)
         index = np.argmin(dist)
         action = arrow[index]
+        self.next_state = self.obs_to_state(obs)
         return action
 
     def get_T_from_M(self, M):
+        """
+        Compute the transition matrix from the computationally simulated successor matrix M
+        Parameters
+        ----------
+        M: array (n_state,n_state)
+            Successor representation matrix M
+
+        Returns
+        -------
+        T: array (n_state,n_state)
+             The computed transition matrix from the successor representation matrix M
+        """
         T=(1/self.gamma)*np.linalg.inv(M)@(M-np.eye(self.n_state))
         return T
 
-    def create_transmat(self, state_density, name_env, plotting_varible=True):
-        global transmat_norm
+    def create_transmat(self, state_density, name_env, plotting_variable=True):
+
+        """
+        Creates the normalised transition matrix for a rectangular environment '2D_env'
+
+        Parameters
+        ----------
+            state_density: float
+                density of SR-agent states (should be proportional to the step-size)
+            name_env: string
+                name of the environment to create ( There is only one environment type for now)
+                If a new state space type is added please update the action function accordingly
+            plotting_variable: bool
+                If True: plots the normalised transition matrix
+        Returns:
+        -------
+            transmat_norm: array (n_state,n_state)
+                Normalised transition matrix
+
+         """
 
         if name_env == '2D_env':
-            '''
-            TaskGraph for rectangular environment
-
-            Args:
-                param: dictionary {'rec_length': length,'rec_width': width, 'lattice': 'sqaure' / 'tri'}
-
-            Returns:y
-                adjmat: adjacency matrix
-                node2xy: xy coordinates of each state for plotting (via networkx)
-                groups: group labels for each state, always []
-            '''
-
-
 
             adjmat_triu = np.zeros((self.n_state, self.n_state))
-            node2xy = []
             node_layout = np.arange(self.n_state).reshape(self.l, self.w)
             x_coord = np.linspace(0, np.min([self.l / self.w, 1]), num=self.l)
             y_coord = np.linspace(0, np.min([self.w / self.l, 1]), num=self.w)
@@ -151,44 +267,38 @@ class SR(NeuralResponseModel):
             row_sums = np.sum(transmat, axis=1)
             row_sums[row_sums == 0] = 1
             self.transmat_norm = transmat / row_sums.reshape(-1, 1)
-            # Initial srmat
 
+        # Initial srmat
         self.srmat = np.eye(self.n_state)
 
-        if plotting_varible==True:
+        if plotting_variable==True:
             f, ax = plt.subplots(1, 1, figsize=(14, 5))
             ax.imshow(self.transmat_norm, interpolation='nearest')
 
         return self.transmat_norm
 
     def update_successor_rep(self):
-        '''
-        Compute the successor representation matrix using geometric sums
-
-        Args:
-            transmat: (n_state, n_state) numpy array, transition matrix
-            gamma: scalar, discounting factor
+        """
+        Compute the successor representation matrix using geometric sums.
 
         Returns:
-            srmat: (n_state, n_state) numpy array, successor representation matrix
-        '''
+        -------
+            srmat_ground: (n_state, n_state) numpy array,
+                Successor representation matrix
+        """
         transmat_type = np.array(self.transmat_norm, dtype=np.float64)
 
         self.srmat_ground = np.linalg.inv(np.eye(self.n_state) - self.gamma * transmat_type)
         return self.srmat_ground
 
     def successor_rep_sum(self):
-        '''
+        """
         Compute the successor representation using successive additive update
 
-        Args:
-            transmat: (n_state, n_state) numpy array, transition matrix
-            gamma: scalar discount rate
-            threoshold: scalar, upper bound for the update size
-
         Returns:
-            srmat: (n_state, n_state) numpy array, successor representation matrix
-        '''
+        -------
+            srmat_sum: (n_state, n_state) numpy array, successor representation matrix
+        """
 
         self.srmat_sum = np.zeros_like(self.transmat_norm)
         keep_going = True
@@ -201,47 +311,51 @@ class SR(NeuralResponseModel):
 
         return self.srmat_sum
 
-    def update_successor_rep_td(self,obs,curr_state):
+
+    def update(self):
         """
-        Compute the successor representation matrix using TD learning
-        Args:
-            transmat: (n_state, n_state) numpy array, transition matrix
+        Compute the successor representation matrix using TD learning while interacting with the environement
+
         Returns:
+        -------
             srmat: (n_state, n_state) successor representation matrix
-            srmat_snapshots: dictionary, recording srmat at different time steps during learning
         """
-        next_state = self.obs_to_state(obs)
+
+        if self.inital_obs_variable is None:
+            self.curr_state= self.next_state
+            self.inital_obs_variable=True
+
+        next_state = self.next_state
         self.n_state = self.transmat_norm.shape[0]
-        a = np.array(curr_state)
+        a = np.array(self.curr_state)
         x = a.flatten()
         b = np.eye(self.n_state)[x, :self.n_state]
         L = b.reshape(a.shape + (self.n_state,))
         curr_state_vec = L
 
-        self.srmat[:, curr_state] = self.srmat[:, curr_state] + self.learning_rate * (curr_state_vec +
-                                                                                self.gamma * self.srmat[:,
-                                                                                             next_state] - self.srmat[:,
-                                                                                                                  curr_state])
+        self.srmat[:, self.curr_state] = self.srmat[:, self.curr_state] + self.learning_rate * (curr_state_vec +
+                                                                                    self.gamma * self.srmat[:,
+                                                                                                 next_state] - self.srmat[:,
+                                                                                                                      self.curr_state])
+        self.curr_state=next_state
 
-        return next_state,self.srmat
+        return self.srmat
 
     def update_successor_rep_td_full(self):
         """
         Compute the successor representation matrix using TD learning
 
-        Args:
-            transmat: (n_state, n_state) numpy array, transition matrix
-
-
         Returns:
-            srmat: (n_state, n_state) successor representation matrix
-            srmat_snapshots: dictionary, recording srmat at different time steps during learning
+        ----------
+            srmat_full: (n_state, n_state)
+                successor representation matrix
+
         """
         random_state = np.random.RandomState(1234)
 
         t_elapsed = 0
         srmat0 = np.eye(self.n_state)
-        srmat = srmat0.copy()
+        srmat_full = srmat0.copy()
         for i in range(self.n_episode):
 
             curr_state = random_state.randint(self.n_state)
@@ -255,16 +369,27 @@ class SR(NeuralResponseModel):
                 random_state.multinomial(1, self.transmat_norm[curr_state, :])
                 next_state = np.where(random_state.multinomial(1, self.transmat_norm[curr_state, :]))[0][0]
 
-                srmat[:, curr_state] = srmat[:, curr_state] + self.learning_rate * (curr_state_vec +
-                                                                                    self.gamma * srmat[:,
-                                                                                                 next_state] - srmat[:,
+                srmat_full[:, curr_state] = srmat_full[:, curr_state] + self.learning_rate * (curr_state_vec +
+                                                                                    self.gamma * srmat_full[:,
+                                                                                                 next_state] - srmat_full[:,
                                                                                                                curr_state])
                 curr_state = next_state
                 t_elapsed += 1
 
-        return srmat
+        return srmat_full
 
-    def plot_trantion(self,matrix, save_path=None, ax=None):
+    def plot_transition(self,matrix, save_path=None, ax=None):
+        """"
+        Plot the input matrix and compare it to the transition matrix from the rectangular environment states space (rectangular- transmat).
+        (If a new state space type is added please update this function)
+        Parameters
+        ----------
+        matrix: array
+            The matrix that will be plotted
+        save_path: string
+            Path to save the plot
+        """
+
         evals, evecs = np.linalg.eig(matrix)
         if ax is None:
             f, ax = plt.subplots(1,2, figsize=(14, 5))
@@ -275,10 +400,17 @@ class SR(NeuralResponseModel):
             plt.close("all")
         return ax
 
+
     def plot_eigen(self,matrix, save_path, ax=None):
         """"
+        Plot the matrix and the 4 largest modes of its eigen-decomposition
+
         Parameters
         ----------
+        matrix: array
+            The matrix that will be plotted
+        save_path: string
+            Path to save the plot
         """
         evals, evecs = np.linalg.eig(matrix)
         if ax is None:
