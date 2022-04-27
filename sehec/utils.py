b9433617dd8853af1a47d8b81241ffedee088f12
rodrigcd <rodrigo.carrasco.davis@gmail.com>
2022-04-27 14:51:47 +0100

    commenting utils function

--- sehec/utils.py
+++ sehec/utils.py
@@ -2,7 +2,29 @@ import numpy as np
 
 
 def check_crossing_wall(pre_state, new_state, wall, wall_closenes=1e-5):
+    """
 
+    Parameters
+    ----------
+    pre_state : (2,) 2d-ndarray
+        2d position of pre-movement
+    new_state : (2,) 2d-ndarray
+        2d position of post-movement
+    wall : (2, 2) ndarray
+        [[x1, y1], [x2, y2]] where (x1, y1) is on limit of the wall, (x2, y2) second limit of the wall
+    wall_closenes : float
+        how close the agent is allowed to be from the wall
+
+    Returns
+    -------
+    new_state: (2,) 2d-ndarray
+        corrected new state. If it is not crossing the wall, then the new_state stays the same, if the state cross the
+        wall, new_state will be corrected to a valid place without crossing the wall
+    cross_wall: bool
+        True if the change in state cross a wall
+    """
+
+    # Check if the line of the wall and the line between the states cross
     A = np.stack([np.diff(wall, axis=0)[0, :], -new_state+pre_state], axis=1)
     b = pre_state - wall[0, :]
     intersection = np.linalg.inv(A) @ b
