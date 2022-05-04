76e195f4ed9ad58a5164d540ccd4f49e5cfd3e15
rodrigcd <rodrigo.carrasco.davis@gmail.com>
2022-05-04 18:39:34 +0100

    custom wall system added, need to re-write merging room environment

--- sehec/envs/arenas/connected_rooms.py
+++ sehec/envs/arenas/connected_rooms.py
@@ -35,79 +35,17 @@ class ConnectedRooms(Simple2D):
         self.singleroom_xsize = singleroom_xsize
         self.door_size = door_size
 
-        env_kwargs["room_width"] = 2*singleroom_xsize
-        env_kwargs["room_depth"] = corridor_ysize + singleroom_ysize
+        env_kwargs["arena_x_limits"] = np.array([-self.singleroom_xsize, self.singleroom_xsize])
+        env_kwargs["arena_y_limits"] = np.array([-self.singleroom_ysize, self.corridor_ysize])
 
         super().__init__(environment_name, **env_kwargs)
 
-        self.arena_limits = np.array([[-self.singleroom_xsize, self.singleroom_xsize],
-                                      [-self.singleroom_ysize, self.corridor_ysize]])
-
-        self.define_walls()
-
-    def create_default_walls(self):
-        self.wall_list.append(np.array([[self.arena_limits[0, 0], self.arena_limits[1, 0]],
-                                        [self.arena_limits[0, 0], self.arena_limits[1, 1]]]))
-        self.wall_list.append(np.array([[self.arena_limits[0, 1], self.arena_limits[1, 0]],
-                                        [self.arena_limits[0, 1], self.arena_limits[1, 1]]]))
-        self.wall_list.append(np.array([[self.arena_limits[0, 0], self.arena_limits[1, 0]],
-                                        [self.arena_limits[0, 1], self.arena_limits[1, 0]]]))
-        self.wall_list.append(np.array([[self.arena_limits[0, 0], self.arena_limits[1, 1]],
-                                        [self.arena_limits[0, 1], self.arena_limits[1, 1]]]))
-
-    def define_walls(self):
-        self.wall_list = []
+    def _create_custom_walls(self):
+        self.custom_walls = []
         # Walls from limit
-        self.create_default_walls()
 
-        self.wall_list.append(np.array([[0, 0], [0, -self.singleroom_ysize]]))
-        self.wall_list.append(np.array([[-self.singleroom_xsize, 0], [-(self.singleroom_xsize/2+self.door_size/2), 0]]))
-        self.wall_list.append(np.array([[-(self.singleroom_xsize/2-self.door_size/2), 0], [0, 0]]))
-        self.wall_list.append(np.array([[0, 0], [self.singleroom_xsize/2-self.door_size/2, 0]]))
-        self.wall_list.append(np.array([[self.singleroom_xsize/2+self.door_size/2, 0], [self.singleroom_xsize, 0]]))
-
-    def step(self, action):
-        self.global_steps += 1
-        action = action/np.linalg.norm(action)
-        new_state = self.state + self.agent_step_size*action
-        new_state, valid_action = self.validate_action(self.state, action, new_state)
-        reward = 0  # If you get reward, it should be coded here
-        transition = {"action": action, "state": self.state, "next_state": new_state,
-                      "reward": reward, "step": self.global_steps}
-        self.history.append(transition)
-        self.state = new_state
-        observation = self.make_observation()
-        return observation, new_state, reward
-
-    def validate_action(self, pre_state, action, new_state):
-        """
-
-        Parameters
-        ----------
-        pre_state : (2,) 2d-ndarray
-            2d position of pre-movement
-        new_state : (2,) 2d-ndarray
-            2d position of post-movement
-
-        Returns
-        -------
-        new_state: (2,) 2d-ndarray
-            corrected new state. If it is not crossing the wall, then the new_state stays the same, if the state cross the
-            wall, new_state will be corrected to a valid place without crossing the wall
-        valid_action: bool
-            True if the change in state cross a wall
-        """
-        valid_action = True
-        for wall in self.wall_list:
-            new_state, new_valid_action = check_crossing_wall(pre_state=pre_state, new_state=new_state, wall=wall)
-            valid_action = new_valid_action and valid_action
-        return new_state, valid_action
-
-    def plot_trajectory(self, history_data=None, ax=None):
-        if len(self.wall_list) != 0:
-            ax = super().plot_trajectory(history_data=history_data, center_room=False)
-            for wall in self.wall_list:
-                ax.plot(wall[:, 0], wall[:, 1], "b", lw=3)
-            return ax
-        else:
-            return super().plot_trajectory(history_data=self.history, center_room=False)
+        self.custom_walls.append(np.array([[0, 0], [0, -self.singleroom_ysize]]))
+        self.custom_walls.append(np.array([[-self.singleroom_xsize, 0], [-(self.singleroom_xsize/2+self.door_size/2), 0]]))
+        self.custom_walls.append(np.array([[-(self.singleroom_xsize/2-self.door_size/2), 0], [0, 0]]))
+        self.custom_walls.append(np.array([[0, 0], [self.singleroom_xsize/2-self.door_size/2, 0]]))
+        self.custom_walls.append(np.array([[self.singleroom_xsize/2+self.door_size/2, 0], [self.singleroom_xsize, 0]]))
