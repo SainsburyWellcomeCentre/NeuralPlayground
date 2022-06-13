0f5fb00f2f8211c815888c203af91b5d6d8dd3c4
rodrigcd <rodrigo.carrasco.davis@gmail.com>
2022-06-13 16:35:45 +0100

    config files and available models to do cross run

--- examples/cross_comparison.py
+++ examples/cross_comparison.py
@@ -1,5 +1,5 @@
-from sehec.utils import inheritors
+from sehec.crossrun.crossrun import CrossRun
 from sehec.models.modelcore import NeuralResponseModel
 
 if __name__ == "__main__":
-    print(NeuralResponseModel.__subclasses__())
+    crossrun = CrossRun()
--- sehec/crossrun/crossrun.py
+++ sehec/crossrun/crossrun.py
@@ -1,9 +1,19 @@
 from ..models.modelcore import NeuralResponseModel
-from ..utils import inheritors
+from ..experimentconfig import cfg
+
 
 class CrossRun(object):
 
-    def __init__(self):
+    def __init__(self, config_file=None):
+        print("Init cross run")
+        if config_file is None:
+            self.config_file = cfg
+        else:
+            self.config_file = config_file
         self._list_available_models()
 
     def _list_available_models(self):
+        print("Models to run: ")
+        for key in self.config_file.available_params:
+            if "model" in key:
+                print(self.config_file.__dict__[key].config_id)
