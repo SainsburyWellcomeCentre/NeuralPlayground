80da6ab9deb059f70f0262a4172d3a174a71fc56
rodrigcd <rodrigo.carrasco.davis@gmail.com>
2022-10-18 23:20:14 +0100

    adding paths to ini for easy access, this will break some things

--- sehec/arenas/__init__.py
+++ sehec/arenas/__init__.py
@@ -1,1 +1,5 @@
-name = 'envs'
+name = 'arenas'
+from .arena_core import Environment
+from .connected_rooms import ConnectedRooms
+from .merging_rooms import MergingRoom2D
+from .simple2d import Simple2D, Sargolini2006, Hafting2008, FullHaftingData, FullSargoliniData
---
+++ sehec/experiments/__init__.py
@@ -0,0 +1,3 @@
+from .hafting_2008_data import FullHaftingData
+from .sargolini_2006_data import SargoliniData, FullSargoliniData
+from .wernle_2018_data import WernleData
