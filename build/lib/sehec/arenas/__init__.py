ac99d29298a1f2fc9ba40064d9dfe634b57a98eb
rodrigcd <rodrigo.carrasco.davis@gmail.com>
2022-11-04 15:46:26 +0000

    Revert "update of repo to match main structure"
    This reverts commit 011037ea8e23a11b4ce76e90a99d84a0ea1df031.

--- build/lib/sehec/arenas/__init__.py
+++
@@ -1,7 +0,0 @@
-name = 'arenas'
-from .arena_core import Environment
-from .connected_rooms import ConnectedRooms
-from .merging_rooms import MergingRoom2D
-from .simple2d import Simple2D
-from .hafting_2008 import Hafting2008
-from .sargolini_2006 import Sargolini2006, BasicSargolini2006
