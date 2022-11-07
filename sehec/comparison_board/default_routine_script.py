6572d41d380bd00a0b28a6d81779273e811fd726
rodrigcd <rodrigo.carrasco.davis@gmail.com>
2022-11-07 12:55:54 +0000

    sehec to neuralplayground

--- sehec/comparison_board/default_routine_script.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-
-from sehec.comparison_board.default_run import default_run
-import sys
-
-if __name__ == "__main__":
-    default_run(sys.argv[1])
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="sehec",
+    name="neuralplayground",
     version="0.0.1",
     author="clem_domine_rod_carrasco",
     author_email="rodrigo.carrasco.davis@gmail.com",
