4a6729c0d84d2285c87ae36106ccab6c161203cb
rodrigcd <rodrigo.carrasco.davis@gmail.com>
2022-11-07 16:54:53 +0000

    refactoring

--- neuralplayground/experiments/__init__.py
+++ neuralplayground/experiments/__init__.py
@@ -1,3 +1,3 @@
 from .hafting_2008_data import Hafting2008Data
 from .sargolini_2006_data import SargoliniData, Sargolini2006Data
-from .wernle_2018_data import Wernle20118Data
+from .wernle_2018_data import Wernle2018Data
