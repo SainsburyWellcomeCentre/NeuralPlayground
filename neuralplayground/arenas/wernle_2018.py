ce4c7aae68b425c2dbad691acfc75dea567174e9
rodrigcd <rodrigo.carrasco.davis@gmail.com>
2022-11-15 14:50:45 +0000

    arenas example notebook

--- neuralplayground/arenas/wernle_2018.py
+++ neuralplayground/arenas/wernle_2018.py
@@ -4,7 +4,7 @@ from ..experiments.wernle_2018_data import Wernle2018Data
 
 
 class Wernle2018(Simple2D):
-    def __init__(self, environment_name="MergingRoom", merge_time=100, switch_time=50, verbose=False, **env_kwargs):
+    def __init__(self, environment_name="MergingRoom", merge_time=100, switch_time=50, verbose=False, data_path=None, **env_kwargs):
         self.environment_name = environment_name
         env_kwargs["arena_x_limits"] = np.array([-100, 100])
         env_kwargs["arena_y_limits"] = np.array([-100, 100])
@@ -12,6 +12,7 @@ class Wernle2018(Simple2D):
         self.merge_time = (merge_time*60) / self.time_step_size
         self.switch_time = (switch_time*60) / self.time_step_size
         self.run_full_experiment = True
+        self.data_path = data_path
         self.data = Wernle2018Data(data_path=self.data_path, verbose=verbose)
         super().__init__(environment_name, **env_kwargs)
         self.AB_id = "AB"
@@ -29,7 +30,7 @@ class Wernle2018(Simple2D):
                           np.random.uniform(low=0, high=self.arena_limits[1, 1])]
         elif room_id == self.AB_id:
             if len(self.custom_walls) != 0:
-                self.custom_walls.pop()
+                self.custom_walls = []
                 self.wall_list = self.default_walls + self.custom_walls
 
     def _create_custom_walls(self):
