797cbb7e7adc55111d35e63cbd0603fd9e852ce0
rodrigcd <rodrigo.carrasco.davis@gmail.com>
2022-11-19 20:43:28 +0000

    almost done with the figure for the abstract ;)

--- neuralplayground/agents/__init__.py
+++ neuralplayground/agents/__init__.py
@@ -1,4 +1,4 @@
 name = 'models'
 from .agent_core import AgentCore, RandomAgent, LevyFlightAgent
-from .stachenfeld_2018 import SR
-from .weber_2018 import ExcInhPlasticity
+from .stachenfeld_2018 import Stachenfeld2018
+from .weber_2018 import Weber2018
