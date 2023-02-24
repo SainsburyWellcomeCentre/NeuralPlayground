ebf644e725c25d41e8e0f50e450ccc9900716675
rodrigcd <rodrigo.carrasco.davis@gmail.com>
2023-02-24 01:57:14 +0000

    changing setup

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="NeuralPlayground: A Standardised Environment for Evaluating Models of Hippocampus and Entorhinal Cortex",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/ClementineDomine/EHC_model_comparison",
+    url="https://github.com/ClementineDomine/NeuralPlayground",
     packages=setuptools.find_packages(),
     classifiers=(
         "Programming Language :: Python :: 3",
