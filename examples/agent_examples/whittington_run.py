192622c11a13986fcf1dbbb0025e5a18dcad24df
LukeHollingsworth <lukehollingsworth99@hotmail.com>
2023-03-02 15:02:45 +0000

    PyTorch version of TEM files

--- examples/agent_examples/whittington_run.py
+++ examples/agent_examples/whittington_run.py
@@ -1,19 +1,20 @@
 import numpy as np
 import torch
+import time
 import matplotlib.pyplot as plt
 import os, shutil
 import tqdm
 from neuralplayground.arenas.torch_TEM_env import TEM_env
-from neuralplayground.agents.torch_TEM import Model
-from neuralplayground.agents.TEM_extras.torch_TEM_parameters import parameters
-from neuralplayground.agents.TEM_extras.torch_TEM_utils import *
+from neuralplayground.agents.whittington_2020 import TEM
+import neuralplayground.agents.TEM_extras.TEM_parameters as parameters
 
-pars_orig = parameters()
+pars_orig = parameters.parameters()
 params = pars_orig.copy()
 
-room_width = [-10, 10]
-room_depth = [-10, 10]
+room_width = [-5, 5]
+room_depth = [-5, 5]
 env_name = "env_example"
+mod_name = "TorchTEMTest"
 time_step_size = 1
 agent_step_size = 1
 
@@ -23,18 +24,18 @@ env = TEM_env(environment_name=env_name,
               arena_y_limits=room_depth,
               time_step_size=time_step_size,
               agent_step_size=agent_step_size)
-agent = Model(params)
+agent = TEM(model_name=mod_name, params=params)
 
+walk, obs, actions = env.batch_reset(normalize_step=False, random_state=True)
 for i in range(params['train_it']):
-    walk, obs, actions = env.batch_reset(normalize_step=False, random_state=True)
-    for j in range(params['n_walks'][i]):
-        agent.act(i, j, walk, obs, actions)
-        walk, obs, actions = env.batch_step(normalize_step=False)
-        print('yess')
+    print(i)
+    agent.act(i, walk, obs, actions)
+    walk, obs, actions = env.batch_step(normalize_step=False)
+    
 
 ax = env.plot_batch_trajectory()
 fontsize = 18
 ax.grid()
 ax.set_xlabel("width", fontsize=fontsize)
 ax.set_ylabel("depth", fontsize=fontsize)
-plt.show()
+plt.savefig('trajectory.png')
