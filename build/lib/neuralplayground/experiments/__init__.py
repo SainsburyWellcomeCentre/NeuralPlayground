1ff6041941218683e8a4feb7b8bf30cef46ba2e3
clementine <clementine.domine98@gmail.com>
2023-03-04 00:34:55 +0000

    remove build from the repo

--- build/lib/neuralplayground/experiments/__init__.py
+++
@@ -1,4 +0,0 @@
-from .experiment_core import Experiment
-from .hafting_2008_data import Hafting2008Data
-from .sargolini_2006_data import SargoliniDataTrajectory, Sargolini2006Data
-from .wernle_2018_data import Wernle2018Data
--- build/lib/neuralplayground/experiments/experiment_core.py
+++
@@ -1,3 +0,0 @@
-class Experiment(object):
-    """Main experiment class, created just for consistency purposes"""
-    pass
