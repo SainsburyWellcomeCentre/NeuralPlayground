f6f1443b713775c3ca22489f40f50b0be9928bad
clementine <clementine.domine98@gmail.com>
2023-07-12 18:29:40 +0100

    fix hafting

--- debugging/dev_backend_runs.py
+++ debugging/dev_backend_runs.py
@@ -1,5 +1,12 @@
 from neuralplayground.backend import SimulationManager
-from neuralplayground.backend.default_simulation import sim_object1, sim_object2
+from neuralplayground.backend.default_simulation import (
+    sim_object1,
+    sim_object2,
+    sim_object3,
+    sim_object4,
+    sim_object5,
+    sim_object6,
+)
 
 
 def main():
@@ -21,7 +28,7 @@ def main():
     # sim_object2.run_sim("try_results_sim4/")
 
     sim_manager = SimulationManager(
-        [sim_object1, sim_object2],
+        [sim_object1, sim_object2, sim_object3, sim_object4, sim_object5, sim_object6],
         runs_per_sim=5,
         manager_id="test_dev",
         verbose=True,
