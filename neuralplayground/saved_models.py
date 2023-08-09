d59bdba922646f2c3fe0c774377615718986ad3a
clementine <clementine.domine98@gmail.com>
2023-08-09 16:32:33 +0100

    update naming

--- neuralplayground/saved_models.py
+++ neuralplayground/saved_models.py
@@ -22,14 +22,14 @@ DATASET_REGISTRY = pooch.create(
     path=LOCAL_DATA_DIR,
     base_url=f"{DATA_URL}/data/",
     registry={
-        "Weber_2018_in_simple2D.zip": "85028da0cc7d657933b0041458e9b61149ad86142d370808eabdd3478f8c1cca",
-        "Weber_2018_in_Hafting2008.zip": "7bc345632c8cb45b04ed7be738afd3b740259d3d3fb1973d441e920ceeb66e1d",
-        "Weber_2018_in_Sargolini2006.zip": "3c3ac58a7ef45a560a8a92b49c31e7724f6d5610c223de6fcf80ed1200a410ab",
+        "weber_2018_in_simple2D.zip": "85028da0cc7d657933b0041458e9b61149ad86142d370808eabdd3478f8c1cca",
+        "weber_2018_in_hafting2008.zip": "7bc345632c8cb45b04ed7be738afd3b740259d3d3fb1973d441e920ceeb66e1d",
+        "weber_2018_in_sargolini2006.zip": "3c3ac58a7ef45a560a8a92b49c31e7724f6d5610c223de6fcf80ed1200a410ab",
         "stachenfeld_2018_in_simple2D.zip": "b0d2db8e2b5ee2d7a4f9b8ea1ba1366dafbbe72bb213363a8536fe930649487e",
-        "stachenfeld_2018_in_Sargolini2006.zip": "7cadfbe2024d5e3ccbae13f9a2ba492791c3a5f82f740e6289c5eadd3d8d63d3",
-        "stachenfeld_2018_in_Hafting2008.zip": "f014be0d6f399b50ebc67e1c4530db24f76ce12fa210aeff2695717598517137",
-        "weber_2018_in_Wernle.zip": "78770840f56e8c7fb22182aa2530027e44be5a9eb9127cdcd291d86bbb3e6623",
-        "weber_2018_in_Merging_Room.zip": "2aed903a302d0965637c42ab36a9547d2e824124872595af6ba1d763cc381531",
+        "stachenfeld_2018_in_sargolini2006.zip": "7cadfbe2024d5e3ccbae13f9a2ba492791c3a5f82f740e6289c5eadd3d8d63d3",
+        "stachenfeld_2018_in_hafting2008.zip": "f014be0d6f399b50ebc67e1c4530db24f76ce12fa210aeff2695717598517137",
+        "weber_2018_in_wernle.zip": "78770840f56e8c7fb22182aa2530027e44be5a9eb9127cdcd291d86bbb3e6623",
+        "weber_2018_in_merging_room.zip": "2aed903a302d0965637c42ab36a9547d2e824124872595af6ba1d763cc381531",
         # noqa: E501# noqa: E501
     },
 )
