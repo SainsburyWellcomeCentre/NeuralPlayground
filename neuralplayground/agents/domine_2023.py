03bf48a5e03b3d6bd39974552cafbbf083a18cb6
clementine <clementine.domine98@gmail.com>
2024-01-09 22:18:18 +0100

    update

--- neuralplayground/agents/domine_2023.py
+++ neuralplayground/agents/domine_2023.py
@@ -112,7 +112,7 @@ class Domine2023(
         if self.wandb_on:
             dateTimeObj = datetime.now()
             wandb.init(
-                project="graph-delaunay_5",
+                project="graph-delaunay_small",
                 entity="graph-brain",
                 name=experiment_name + dateTimeObj.strftime("%d%b_%H_%M_%S"),
             )
