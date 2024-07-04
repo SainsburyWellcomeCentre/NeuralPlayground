aebb6fba246f771f978c0132523b42d91d0c7de8
LukeHollingsworth <lukehollingsworth99@hotmail.com>
2024-07-04 12:00:46 +0100

    huge 50K run added

--- examples/agent_examples/whittington_2020_run.py
+++ examples/agent_examples/whittington_2020_run.py
@@ -14,8 +14,8 @@ from neuralplayground.backend import SingleSim, tem_training_loop
 from neuralplayground.experiments import Sargolini2006Data
 
 # Set the location for saving the results of the simulation
-simulation_id = "TEM_big_50K"
-save_path = os.path.join(os.getcwd(), "results_big_50K")
+simulation_id = "TEM_huge_50K"
+save_path = os.path.join(os.getcwd(), "results_huge_50K")
 # save_path = os.path.join(os.getcwd(), "examples", "agent_examples", "trained_results")
 agent_class = Whittington2020
 env_class = BatchEnvironment
@@ -26,40 +26,40 @@ full_agent_params = params.copy()
 
 # Set the x and y limits for the arena
 arena_x_limits = [
-    [-10, 10],
-    [-8, 8],
-    [-10, 10],
-    [-12, 12],
-    [-8, 8],
-    [-10, 10],
-    [-12, 12],
-    [-10, 10],
-    [-8, 8],
-    [-10, 10],
-    [-12, 12],
-    [-10, 10],
-    [-8, 8],
-    [-10, 10],
-    [-12, 12],
-    [-10, 10],
+    [-20, 20],
+    [-16, 16],
+    [-20, 20],
+    [-24, 24],
+    [-16, 16],
+    [-20, 20],
+    [-24, 24],
+    [-20, 20],
+    [-16, 16],
+    [-20, 20],
+    [-24, 24],
+    [-20, 20],
+    [-16, 16],
+    [-20, 20],
+    [-24, 24],
+    [-20, 20],
 ]
 arena_y_limits = [
-    [-10, 10],
-    [-8, 8],
-    [-10, 10],
-    [-12, 12],
-    [-8, 8],
-    [-10, 10],
-    [-12, 12],
-    [-10, 10],
-    [-8, 8],
-    [-10, 10],
-    [-12, 12],
-    [-10, 10],
-    [-8, 8],
-    [-10, 10],
-    [-12, 12],
-    [-10, 10],
+    [-20, 20],
+    [-16, 16],
+    [-20, 20],
+    [-24, 24],
+    [-16, 16],
+    [-20, 20],
+    [-24, 24],
+    [-20, 20],
+    [-16, 16],
+    [-20, 20],
+    [-24, 24],
+    [-20, 20],
+    [-16, 16],
+    [-20, 20],
+    [-24, 24],
+    [-20, 20],
 ]
 
 # Set parameters for the environment that generates observations
@@ -105,7 +105,7 @@ agent_params = {
 }
 
 # Full model training consists of 20000 episodes
-training_loop_params = {"n_episode": 10, "params": full_agent_params}
+training_loop_params = {"n_episode": 50000, "params": full_agent_params}
 
 # Create the training simulation object
 sim = SingleSim(
