712831359aea6c66f28a3816c120c3360801fcdc
LukeHollingsworth <lukehollingsworth99@hotmail.com>
2024-07-11 13:55:02 +0100

    small TEM run

--- examples/agent_examples/whittington_slurm.sh
+++ examples/agent_examples/whittington_slurm.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # Set the job name variable
-#SBATCH --job-name=TEM_big_double_sd
+#SBATCH --job-name=TEM_small
 #SBATCH --mem=50000 # memory pool for all cores
 #SBATCH --time=72:00:00 # time
-#SBATCH -o TEM_logs/TEM_big_double_sd.%N.%j.out # STDOUT
-#SBATCH -e TEM_logs/TEM_big_double_sd.%N.%j.err # STDERR
+#SBATCH -o TEM_logs/TEM_small.%N.%j.out # STDOUT
+#SBATCH -e TEM_logs/TEM_small.%N.%j.err # STDERR
 #SBATCH -p gpu
 #SBATCH --gres=gpu:1
 #SBATCH --ntasks=1
