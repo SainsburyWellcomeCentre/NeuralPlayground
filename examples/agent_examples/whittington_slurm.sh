74990a661586bacb5bc84fa31f9e65d9cfade573
LukeHollingsworth <lukehollingsworth99@hotmail.com>
2024-07-16 18:07:23 +0100

    trying cpu slurm

--- examples/agent_examples/whittington_slurm.sh
+++ examples/agent_examples/whittington_slurm.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
-
-#SBATCH --job-name=TEM_update_test
-#SBATCH --mem=20000
-#SBATCH --time=72:00:00
-#SBATCH -o TEM_logs/TEM_update_test.%N.%j.out
-#SBATCH -e TEM_logs/TEM_update_test.%N.%j.err
 #SBATCH -p cpu
-#SBATCH --ntasks=1
+#SBATCH --nodes 1
+#SBATCH --cpus-per-task 4
+#SBATCH --mem 16
+#SBATCH --time=1:00
 
 source ~/.bashrc
 
 conda activate NPG-env
-print("Test")
+
 python whittington_2020_run.py
 
 exit
