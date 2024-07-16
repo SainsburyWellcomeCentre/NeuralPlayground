a03ada0daa9d51abe99bef8e03e5ad697d64ecbd
LukeHollingsworth <lukehollingsworth99@hotmail.com>
2024-07-16 18:14:41 +0100

    trying cpu slurm

--- examples/agent_examples/whittington_slurm.sh
+++ examples/agent_examples/whittington_slurm.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH -p cpu
-#SBATCH --nodes 1
-#SBATCH --cpus-per-task 4
-#SBATCH --mem 16
+#SBATCH --job-name=TEM_update_test
+#SBATCH --mem=20000
 #SBATCH --time=72:00:00
-#SBATCH --job-name=TEM_update
-#SBATCH -o TEM_logs/TEM_update.%N.%j.out
-#SBATCH -e TEM_logs/TEM_update.%N.%j.err
+#SBATCH -o TEM_logs/TEM_update_test.%N.%j.out
+#SBATCH -e TEM_logs/TEM_update_test.%N.%j.err
+#SBATCH -p gpu
+#SBATCH --gres=gpu:1
+#SBATCH --ntasks=1
 
 source ~/.bashrc
 
