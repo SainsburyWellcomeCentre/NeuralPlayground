a681c6b65e411b1fab93f40f1a302162af40701f
LukeHollingsworth <lukehollingsworth99@hotmail.com>
2024-07-19 14:27:21 +0100

    cpu slurm added

--- examples/agent_examples/whittington_slurm.sh
+++ examples/agent_examples/whittington_slurm.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-#SBATCH --job-name=TEM_update
+#SBATCH --job-name=TEM_cpu
 #SBATCH --mem=20000
 #SBATCH --time=72:00:00
-#SBATCH -o TEM_logs/TEM_update.%N.%j.out
-#SBATCH -e TEM_logs/TEM_update.%N.%j.err
-#SBATCH -p gpu
-#SBATCH --gres=gpu:1
+#SBATCH -o TEM_logs/TEM_cpu.%N.%j.out
+#SBATCH -e TEM_logs/TEM_cpu.%N.%j.err
+#SBATCH -p cpu
 #SBATCH --ntasks=1
 
 source ~/.bashrc
